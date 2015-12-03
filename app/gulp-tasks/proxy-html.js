0d077181f45ddcc24af0c041d2115989c5eebc15
David Thornley <david.thornley@darnt.com>
2015-12-03 14:31:06 +0100

    Remove some incorrect urls and make generation of proxy-html more flexible.

--- app/gulp-tasks/proxy-html.js
+++ app/gulp-tasks/proxy-html.js
@@ -4,10 +4,26 @@ module.exports = function (gulp, deps, taskConfig, fullConfig) {
     gulp.task(
         'proxy-html',
         function () {
+            // First merge the MASTER_URL array values into a string
+            function mergeMasterURL(master, combine) {
+                return combine(_.values(master));
+            }
+
+            if (taskConfig.tokens.hasOwnProperty('MASTER_URL')) {
+                taskConfig.tokens.MASTER_URLS = mergeMasterURL(taskConfig.tokens.MASTER_URL, function (urls) {
+                    var merged = '';
+                    urls.forEach (function (name) {
+                        merged = merged + ' "' + name + '": "*",'
+                    });
+                    merged = merged.slice(0, -1);
+                    return merged;
+                });
+            }
+
             return gulp.src(taskConfig.src)
                 .pipe(deps.tokenReplace({tokens: taskConfig.tokens}))
                 .pipe(deps.rename(taskConfig.name))
                 .pipe(gulp.dest(taskConfig.target));
         }
     );
-}
+};
